55114261d36f325d998e3722b0215f4d4422e043
Rohan Bansal <bansalrohan18@gmail.com>
2021-12-19 21:27:17 -0800

    init

--- bit.py
+++ bit.py
@@ -1,22 +1,48 @@
-import os, sys, subprocess
+import os, sys, subprocess, argparse, time
 from lib.tcolors import tcolors
 from lib.spinner import Spinner
 from lib.colorizer import colorize
 
+path = os.getcwd()
 
-def createRepo():
-    print(tcolors.GREEN + "Creating a new repo..." + tcolors.RESET)
-    subprocess.call(["git", "init"])
-    subprocess.call(["git", "add", "."])
-    subprocess.call(["git", "commit", "-m", "Initial commit"])
-    print(tcolors.GREEN + "Repo created!" + tcolors.RESET)
+def error(message):
+    print(colorize("err: " + message, tcolors.RED_BOLD))
 
-def processArgs(arg):
-    
+def createRepo(message="initial commit"):
+    print(colorize("initializing repository in ", tcolors.GREEN_BOLD) + colorize(path, tcolors.CYAN))
 
-    if arg[1] == "--create":
-        createRepo();
+    if os.path.exists(os.path.join(path, '.git')):
+        response = input(colorize("git repository already exists. reinitialize? [y/n]: ", tcolors.YELLOW))
+        if response == 'n':
+            error('aborted.')
+            return
+        
 
-if __name__ == "__main__":
+    with Spinner(" ", tcolors.CYAN_BOLD):
+        subprocess.check_output(["git", "init", "."], 
+            stderr=subprocess.STDOUT)
+        subprocess.check_output(["git", "add", "."],
+            stderr=subprocess.STDOUT)
+        try:
+            subprocess.check_output(["git", "commit", "-m", message])
+        except subprocess.CalledProcessError:
+            pass
+        time.sleep(0.5)
 
-  processArgs(sys.argv);
+    print(colorize("done.", tcolors.GREEN_BOLD))
+
+
+
+parser = argparse.ArgumentParser()
+subparser = parser.add_subparsers(dest='command')
+
+create = subparser.add_parser('create')
+create.add_argument("-m", type=str, required=False)
+
+args = parser.parse_args()
+
+if args.command == 'create':
+    if(args.m != None):
+        createRepo(args.m)
+    else:
+        createRepo()
