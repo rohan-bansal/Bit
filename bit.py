6fe328487c20dde25c1a98af2258412baf482929
Rohan Bansal <bansalrohan18@gmail.com>
2021-12-19 17:49:18 -0800

    Initial commit

--- bit.py
+++ bit.py
@@ -1,10 +1,22 @@
-import os, platform
-from sys import argv
+import os, sys, subprocess
 from lib.tcolors import tcolors
 from lib.spinner import Spinner
 from lib.colorizer import colorize
 
 
+def createRepo():
+    print(tcolors.GREEN + "Creating a new repo..." + tcolors.RESET)
+    subprocess.call(["git", "init"])
+    subprocess.call(["git", "add", "."])
+    subprocess.call(["git", "commit", "-m", "Initial commit"])
+    print(tcolors.GREEN + "Repo created!" + tcolors.RESET)
+
+def processArgs(arg):
+    
+
+    if arg[1] == "--create":
+        createRepo();
+
 if __name__ == "__main__":
 
-    pass
+  processArgs(sys.argv);
